f890ac36427a68792e159532362805c80ad642da
yauhen_bialetski <yauhen_bialetski@epam.com>
2024-02-29 17:08:27 +0100

    Lesson_13 new test

--- lesson_13/test.js
+++ lesson_13/test.js
@@ -2,4 +2,8 @@ describe('Test', function () {
     test('new test', async () => {
         expect(true).toEqual(true)
     })
+
+    test('another one new test', async () => {
+        expect(10).toEqual(10)
+    })
 })
